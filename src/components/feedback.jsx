ebba30ad582a3aee6c8e1d1e088f1f7797e21522
MRifqiandi <11211063@student.itk.ac.id>
2024-05-09 02:55:31 +0800

    feedback-fix

--- src/components/feedback.jsx
+++ src/components/feedback.jsx
@@ -1,38 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Feedback = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    topic: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = () => {
-    const { name, topic, message } = formData;
-    const email = "v99akun21@gmail.com"; // Ganti dengan email penerima
-    const subject = encodeURIComponent(topic);
-    const body = encodeURIComponent(
-      `Nama: ${name}\n\nKritik dan Saran:\n${message}`
-    );
-    const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
-    window.location.href = mailtoLink;
-  };
-
   return (
     <div className="max-w-md mx-auto mt-8 bg-white rounded-lg shadow-md overflow-hidden mb-8">
       <div className="bg-gray-100 py-4 px-6 border-b border-gray-200">
         <h2 className="text-lg font-semibold text-gray-800">Feedback Form</h2>
       </div>
       <form
+        action="https://formspree.io/f/moqgbaja"
+        method="POST"
         className="px-6 py-8"
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
       >
         <div className="mb-4">
           <label
@@ -45,8 +22,6 @@ const Feedback = () => {
             type="text"
             id="name"
             name="name"
-            value={formData.name}
-            onChange={handleChange}
             className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             required
           />
@@ -62,8 +37,21 @@ const Feedback = () => {
             type="text"
             id="topic"
             name="topic"
-            value={formData.topic}
-            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 font-semibold mb-2"
+          >
+            Email:
+          </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
             className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             required
           />
@@ -78,8 +66,6 @@ const Feedback = () => {
           <textarea
             id="message"
             name="message"
-            value={formData.message}
-            onChange={handleChange}
             rows="4"
             className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
             required
