196adf1b1162204a4e1f269346aef31f31415a52
MasRangga <ranggah503@gmail.com>
2024-05-10 02:08:27 +0800

    fix : plugin bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import PlantPage from "./pages/PlantPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/plantpage" element={<PlantPage />} />
+        <Route path="/aboutpage" element={<AboutPage />} />
       </Routes>
     </Router>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
+
 const HomePage = () => {
   return (
     <div>
       <Navbar />
-      <div className="ml-10">Homepage</div>
+     
     </div>
   );
 };
